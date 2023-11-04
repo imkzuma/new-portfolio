b6d2ef21bf39b7522bcbd93f678fa3319d05ebb2
imkzuma <edu.gungkrisna@gmail.com>
2023-11-04 14:13:35 +0800

    (change): add google cloud deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /*
+    for next export command to generate static html. 
+    it for google cloud app engine
+  */
+  distDir: 'build'
 }
 
 module.exports = nextConfig
